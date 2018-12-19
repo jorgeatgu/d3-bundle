fe1df616d78366001babbb96bd5deeba884bc585
Jorge Aznar <jorgeatgu@gmail.com>
2018-12-19 19:31:02 +0100

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+export {
+    select,selectAll
+} from "d3-selection";
+
+export {
+    area
+} from "d3-shape";
+
+export {
+    scaleTime,scaleLinear
+} from "d3-scale";
+
+export {
+    axisBottom,axisLeft
+} from "d3-axis";
+
+export {
+    csv
+} from "d3-request";
+
+export {
+    easeLinear
+} from "d3-ease";
+
+export {
+    format
+} from "d3-format";
+
+import "d3-transition";
